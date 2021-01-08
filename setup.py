7e9cbb6085356789540a4683858fc09456e1e53d
vikram <vikram.shinde@morrisonsplc.co.uk>
2021-01-08 16:42:30 +0000

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as file:
 
 setuptools.setup(
     name="python-project-vik",
-    version="1.1.0",
+    version="1.1.1",
     author="Vikram Shinde",
     author_email="vik.shinde@gmail.com",
     description="Python project for testing",
